21af40ad14dbef0dac03f01359a9b76419582663
Harri Rekola <harri.rekola@tuni.fi>
2025-04-03 13:51:59 +0300

    luvut.c

---
+++ Osa 1/luvut.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+    int i;
+    int first = 1;
+
+    for(i = 1; i < 101; i++) {
+        if(i % 2 != 0 && i % 3 != 0 && i % 5 != 0) {
+            if (!first) {
+                printf(" ");
+            }
+            printf("%d", i);
+            first = 0;
+        }
+    }
+    printf("\n");
+    return 0;
+}
