c05aa8b82b03ac5fb69cb6a7a446e9599d038272
Harri Rekola <harri.rekola@tuni.fi>
2025-04-03 15:17:41 +0300

    kertotaulu.c

---
+++ Osa 1/kertotaulu.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+    int i;
+    int j;
+
+    printf("   x");
+    for (i = 1; i <= 15; i++) {
+        printf("%4d", i);
+    }
+    printf("\n");
+
+    for(i = 1; i <= 15; i++) {
+        printf("%4d", i);
+        for(j = 1; j <= 15; j++) {
+            printf("%4d", i * j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
