21c5f8fc126f34a9501c725665e7e21f3002d047
Harri <harrihrekola@gmail.com>
2025-04-25 16:41:02 +0300

    muunna

---
+++ Osa 2/muunna/muunna.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+#include "muunna.h"
+
+void muunnaTaulu(unsigned int koko, double* taulu, double (*muunnos)(double))
+{
+  int i;
+  for(i = 0; i < koko; i++) {
+    taulu[i] = muunnos(taulu[i]);
+  }
+}
